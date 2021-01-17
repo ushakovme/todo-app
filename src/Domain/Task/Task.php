c6448e37c2b2e52c49773c19425ec3f4d897a45d
Ilya <whitecomp777@yandex.ru>
2021-01-17 21:41:42 +0300

    adds is completed property

--- src/Domain/Task/Task.php
+++ src/Domain/Task/Task.php
@@ -8,12 +8,13 @@ use JsonSerializable;
 
 final class Task implements JsonSerializable
 {
-    private TaskId $id;
+    private ?TaskId $id;
     private string $content;
+    private bool $isCompleted = false;
 
     const MAX_TASK_LENGTH = 1000;
 
-    public function __construct(TaskId $id, string $content)
+    public function __construct(?TaskId $id, string $content)
     {
         $this->id = $id;
         $this->setContent($content);
@@ -39,6 +40,16 @@ final class Task implements JsonSerializable
     public function getContent(): string
     {
         return $this->content;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this->isCompleted;
+    }
+
+    public function complete()
+    {
+        $this->isCompleted = true;
     }
 
     public function jsonSerialize()
